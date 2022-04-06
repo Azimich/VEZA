e1248626e9486a20b35f0ab2b8da499bad35db8a
Азимов Азимджон Нуралиевич <azimov@veza.moscow>
2022-04-06 11:54:55 +0300

    Add a News page

--- src/js/_equipmentpage.js
+++ src/js/_equipmentpage.js
@@ -4,40 +4,27 @@ export const initEquipmentpage = () => {
 
   function clickHandler (nodeList = []) {
     nodeList.forEach(link => link.addEventListener("click", function() {
-      if(this.parentNode.classList.contains("active")) {
-        this.parentNode.classList.remove("active") 
+      if(this.classList.contains("active")) {
+        this.classList.remove("active")
+        this.parentNode.nextElementSibling.classList.remove("active") 
       } else {
         nodeList.forEach(item => {
-          item.parentNode.classList.remove("active")
+          item.classList.remove("active")
+          item.parentNode.nextElementSibling.classList.remove("active")
         })
-        this.parentNode.classList.add("active")
+        this.classList.add("active")
+        this.parentNode.nextElementSibling.classList.add("active")
       }
     }));
   }
 
-  const firstLevelMenu = document.querySelectorAll('.clicker');
-  const secondLevelMenu = document.querySelectorAll('.clicker__2');
-  const thirdLevelMenu = document.querySelectorAll('.clicker__3');
+  const clicker1 = document.querySelectorAll('.clicker');
+  const clicker2 = document.querySelectorAll('.clicker__2');
+  const clicker3 = document.querySelectorAll('.clicker__3');
 
-  const nodeList = [firstLevelMenu, secondLevelMenu, thirdLevelMenu];
+  const nodeList = [clicker1, clicker2, clicker3];
   nodeList.map(node => clickHandler(node))
-
-  const level1 = document.querySelectorAll(".equipment__sub__menu");
-  const level2 = document.querySelectorAll(".equipment__sub__menu-two");
-  const level3 = document.querySelectorAll(".equipment__sub__menu-three");
 }
 
 
 
-  // firstLevelMenu.forEach(link => link.addEventListener("click", function() {
-  //   if(this.parentNode.classList.contains("active")) {
-  //     this.parentNode.classList.remove("active") 
-  //     level1.forEach(level => level.classList.remove("active"))
-  //   } else {
-  //     firstLevelMenu.forEach(item => {
-  //       item.parentNode.classList.remove("active")
-  //     })
-  //     this.parentNode.classList.add("active")
-  //     level1.forEach(item => item.classList.add("active"))
-  //   }
-  // }));
