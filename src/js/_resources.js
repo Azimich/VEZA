cb90933e1a7f70f6a20ff6bea8dfa234cb4bfec9
Азимов Азимджон Нуралиевич <azimov@veza.moscow>
2022-04-13 17:51:59 +0300

    Разработал страницу контакты с адаптивной версией. И начал  страницу Техническая поддержка

--- src/js/_resources.js
+++ src/js/_resources.js
@@ -1,18 +1,18 @@
 export const initResources = () => {
 
-  // document.querySelectorAll('.company__nav__menu-item-link').forEach((elem) => {
-  //     elem.addEventListener('click', function(e) {
-  //         const id = e.target.getAttribute('href').replace('#','');
-  //         document.querySelectorAll('.company__nav__menu-item-link').forEach(
-  //             (element) => element.classList.remove('active')
-  //         );
-  //         document.querySelectorAll('.tab__cont').forEach(
-  //             (el) => el.classList.add('hide')
-  //         );
-  //         elem.classList.add('active');
-  //         document.getElementById(id).classList.remove('hide');
-  //     });
-  // });
+  document.querySelectorAll('.resources__nav__menu-item-link').forEach((elem) => {
+      elem.addEventListener('click', function(e) {
+          const id = e.target.getAttribute('href').replace('#','');
+          document.querySelectorAll('.resources__nav__menu-item-link').forEach(
+              (element) => element.classList.remove('active')
+          );
+          document.querySelectorAll('.tab__cont').forEach(
+              (el) => el.classList.add('hide')
+          );
+          elem.classList.add('active');
+          document.getElementById(id).classList.remove('hide');
+      });
+  });
   
 
   // Accordions
