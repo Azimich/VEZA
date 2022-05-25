b13aee9e834dcae9dc88c29dfd32013fd1f54f06
Азимов Азимджон Нуралиевич <azimov@veza.moscow>
2022-05-25 20:30:42 +0300

    В процессе реализации карты

--- src/js/_maps.js
+++ src/js/_maps.js
@@ -1,155 +1,78 @@
 export const initMaps = ()=> {
 
-  let moskow = [55.750104, 37.787309];
-  let chehov = [55.158164, 37.467500];
-  let tver = [56.860167, 35.920233];
-  let yaroslav = [57.637897, 39.857926];
-  let veza = [57.012391, 40.987935];
-  let vlad = [56.119945, 40.375706];
+  const $leftLinks = document.querySelectorAll('.left-menu a'),
+		$mapLinks = document.querySelectorAll('.map a'),
+    $tooltip = document.querySelector('.managers__maps-tooltip');
 
+  $leftLinks.forEach(el => {
+    el.addEventListener('mouseenter', (e) => {
+      let self = e.currentTarget;
+      let selfClass = self.getAttribute('href');
+      let color = self.dataset.color;
+      let currentElement = document.querySelector(`.map a[href="${selfClass}"]`);
+      let currentPolygon = currentElement.querySelectorAll('polygon');
+      let currentPath = currentElement.querySelectorAll('path');
+      if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+      if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+      self.classList.add('active');
+    });
+  });
 
-
-
-  function init() {
-    let map = new ymaps.Map('map-test', {
-      center: moskow,
-      zoom: 5,
+  $mapLinks.forEach(el => {
+    el.addEventListener('mouseenter', (e) => {
+      let self = e.currentTarget;
+      let color = self.dataset.color;
+      let currentPolygon = self.querySelectorAll('polygon');
+      let currentPath = self.querySelectorAll('path');
+      if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
+      if (currentPath) currentPath.forEach(el => el.style.cssText = `fill: ${color}; stroke-width: 2px;`);
     });
 
-    let placemark = new ymaps.Placemark(moskow, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА МОСКВА</div>
-        <p>Россия, Москва, Зелёный проспект, 20</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel: +7 495 989-47-20">+7 495 989-47-20</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:msk@veza.ru">msk@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Москва'
-    }, {
+    $mapLinks.forEach(continents => {
+      continents.addEventListener('mousemove', function(e) {
+        $tooltip.innerText = this.dataset.title;
+        $tooltip.style.top = (e.y + 250) + 'px';
+        $tooltip.style.left = (e.x + 30) + 'px';
+        $tooltip.style.fontSize = '12'  + 'px';
+      });
 
-      preset: 'islands#greenDotIconWithCaption'
+      continents.addEventListener('mouseenter', function() {
+        $tooltip.style.display = 'block';
+      });
+
+      continents.addEventListener('mouseleave', function() {
+        $tooltip.style.display = 'none';
+      });
     });
 
-    let placemark1 = new ymaps.Placemark(chehov, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА ЧЕХОВ</div>
-        <p>Россия, Московская область, Чехов, улица Полиграфистов, 1с2 "Б" офис 204 (2 этаж)</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel:+7(496) 727-70-71">+7(496) 727-70-71</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:chehov@veza.ru">chehov@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Чехов'
-    }, {
-      preset: 'islands#greenDotIconWithCaption'
+    el.addEventListener('mouseleave', (e) => {
+      let self = e.currentTarget;
+      let currentPolygon = self.querySelectorAll('polygon');
+      let currentPath = self.querySelectorAll('path');
+      if (currentPolygon) currentPolygon.forEach(el => el.style.cssText = ``);
+      if (currentPath) currentPath.forEach(el => el.style.cssText = ``);
     });
+  });
 
-    let placemark2 = new ymaps.Placemark(tver, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА ТВЕРЬ</div>
-        <p>Россия, Тверь, набережная Степана Разина, 20Б, оф. 27</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel:+7-961-141-86-48">+7-961-141-86-48</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:tver@veza.ru">tver@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Тверь'
-    }, {
-      preset: 'islands#greenDotIconWithCaption'
+
+  // baloons
+  const $baloons = document.querySelectorAll('.baloons');
+  const $closePopup = document.querySelectorAll('.managers__popup-close')
+
+  $baloons.forEach(elem =>{
+    elem.addEventListener('click', function() {
+      elem.classList.toggle('active');
     });
+  });
 
-    let placemark3 = new ymaps.Placemark(yaroslav, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА ТВЕРЬ</div>
-        <p>Россия, Ярославль, улица Чкалова, 2, офис 227</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel:+7 (4852) 92-88-19">+7 (4852) 92-88-19</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:vv@veza.ru">vv@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Ярославль'
-    }, {
-      preset: 'islands#greenDotIconWithCaption'
-    });
+  // $closePopup.forEach(el => el.addEventListener('click', ()=> closePopup()));
 
-    let placemark4 = new ymaps.Placemark(veza, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА ВЕРХОВНОВОЛЖСКИЙ РЕГИОН</div>
-        <p>Россия, Иваново, Шереметевский проспект, 94</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel:+7 (4932) 34-32-87">+7 (4932) 34-32-87</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:vv@veza.ru">vv@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Верхневолжский регион'
-    }, {
-      preset: 'islands#greenDotIconWithCaption'
-    });
+  // function closePopup() {
+  //   $baloons.classList.remove('active');
+  // }
 
-    let placemark5 = new ymaps.Placemark(vlad, {
-      balloonContent: `
-      <div class="ballon">
-        <div class="baloon__address">ВЕЗА ВЛАДИМИР</div>
-        <p>Россия, Владимир, проспект Ленина, 15А</p>
-        <p class="baloon__address-call">Телефоны:</p>
-        <div class="baloon__contacts">
-          <a href="tel:+7(4922) 77-94-92">+7(4922) 77-94-92</a>
-        </div>
-        <p class="baloon__address-call">Электронная почта:</p>
-        <div class="baloon__contacts">
-          <a href="mailto:vv@veza.ru">vv@veza.ru</a>
-        </div>
-      </div>
-      `,
-      iconCaption: 'ВЕЗА Владимир'
-    }, {
-      preset: 'islands#greenDotIconWithCaption'
-    });
+  $closePopup.addEventListener('click', ()=> {
+    $baloons.classList.remove('active');
+  });
 
-
-    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    map.controls.remove('rulerControl'); // удаляем контрол правил
-
-
-    map.geoObjects.add(placemark);
-    map.geoObjects.add(placemark1);
-    map.geoObjects.add(placemark2);
-    map.geoObjects.add(placemark3);
-    map.geoObjects.add(placemark4);
-    map.geoObjects.add(placemark5);
-
-  }
-
-  ymaps.ready(init);
 }
--- src/js/_popup.js
+++ src/js/_popup.js
@@ -17,24 +17,24 @@ export const initPopup = () =>{
   }
 
   const popupCloseIcon = document.querySelectorAll('.close-popup');
-  if (popupCloseIcon.length > 0) {
-    for (let index = 0; index < popupCloseIcon.length; index++) {
-      const el = popupCloseIcon[index];
-      el.addEventListener('click', function (e) {
-        e.preventDefault();
-        popupClose(el.closest('.popup'));
-        document.getElementById('form').reset();
-        bodyUnlock()
-        document.querySelectorAll('input,textarea,.label-file').forEach(item =>{
-          item.style.borderWidth = '1px';
-          item.classList.remove('error');
-          document.querySelector('.checkbox-box>label').classList.remove('error');
-          if(item.classList.contains('label-file')){
-            item.textContent = ''
-            item.style.color = '#6C6D70'
-          }
-        })
-      });
+    if (popupCloseIcon.length > 0) {
+      for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+          e.preventDefault();
+          popupClose(el.closest('.popup'));
+          document.getElementById('form').reset();
+          bodyUnlock()
+          document.querySelectorAll('input,textarea,.label-file').forEach(item =>{
+            item.style.borderWidth = '1px';
+            item.classList.remove('error');
+            document.querySelector('.checkbox-box>label').classList.remove('error');
+            if(item.classList.contains('label-file')){
+              item.textContent = ''
+              item.style.color = '#6C6D70'
+            }
+          })
+        });
+      }
     }
   }
-}
